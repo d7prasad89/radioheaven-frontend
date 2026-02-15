e47aa84b1106276ddfcc39b45beb5489074a6a82
David Prasad <d7prasad89@gmail.com>
2026-02-14 22:30:05 -0500

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 
 import './App.css'
-import { AuthProvider } from './auth/AuthProvider'
 import Dashboard from './dashboard/Dashboard'
 
 function App() {
 
   return (
     <>
-    <AuthProvider>
       <h1 className="text-3xl font-bold mb-4">Radio Heaven</h1>
       <Dashboard />
-      </AuthProvider>
     </>
   )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut as fbSignOut } from "firebase/auth";
 
 const firebaseConfig = { 
-    apiKey: "AIzaSyBxD5quEIATcbTy1zACwgUjr7QsnHG5qZ8",
-    authDomain: "radio-heaven-f0d39.firebaseapp.com",
-    projectId: "radio-heaven-f0d39",
-    storageBucket: "radio-heaven-f0d39.firebasestorage.app",
-    messagingSenderId: "826924310925",
-    appId: "1:826924310925:web:4f6d8985aea0aef0805dc4",
-    measurementId: "G-8RFG9QRPQQ"
+
 };
 
 const app = initializeApp(firebaseConfig);
